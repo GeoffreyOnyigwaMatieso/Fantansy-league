3c3b9f930df5269507cd14af9633b0b356097dc0
Gmatieso <matiesogeoffrey@gmail.com>
2024-12-19 16:50:37 +0300

    refactor: implemented getPlayers & getPlayersFromTeam

--- src/main/java/com/pl/premier_zone/player/PlayerService.java
+++ src/main/java/com/pl/premier_zone/player/PlayerService.java
@@ -1,2 +1,29 @@
-package com.pl.premier_zone.player;public class PlayerService {
+package com.pl.premier_zone.player;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository){
+        this.playerRepository = playerRepository;
+    }
+
+    public List<Player> getPlayers(){
+        return playerRepository.findAll();
+    }
+
+    public List<Player> getPlayersFromTeam(String teamName){
+        return playerRepository.findAll().stream()
+                .filter(player ->  teamName.equals(player.getTeam_name()))
+                .collect(Collectors.toList());
+    }
 }
