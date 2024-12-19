3c4fb81116fddc5fa6b5f6933c71257f0d77cf65
Gmatieso <matiesogeoffrey@gmail.com>
2024-12-19 16:28:04 +0300

    PlayerRepository done

--- src/main/java/com/pl/premier_zone/player/PlayerRepository.java
+++ src/main/java/com/pl/premier_zone/player/PlayerRepository.java
@@ -1,2 +1,13 @@
-package com.pl.premier_zone.player;public interface PlayerRepository {
+package com.pl.premier_zone.player;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, String> {
+    void deleteByName(String player_name);
+    Optional<Player> findByName(String name);
 }
